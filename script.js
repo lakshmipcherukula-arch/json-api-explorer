3ea92d69d729eddcef2b040eb2fecaa5c0ae5374
LakshmiPCherukula <prasannacherukula@gmail.com>
2026-06-23 11:39:39 -0500

    PostForm

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-// so much empty
+// Tasks
+// 1. Fetch and Display Posts
+// ● Use fetch() to retrieve a list of posts from
+// https://jsonplaceholder.typicode.com/posts
+// ● Convert the response to JSON
+// ● Dynamically render the post titles and bodies inside the #postList div// so much empty
+
+const fetchButton = document.getElementById("fetchButton");
+fetchButton.addEventListener('click', () =>{
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+        .then(response => response.json())
+            .then(posts => {
+                const postList = document.getElementById('postList')
+                console.log(posts);
+
+        posts.forEach (post => {
+        const postElement = document.createElement('div');
+        postElement.innerHTML = 
+        `<hr>
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>`;
+        postList.appendChild(postElement);
+    })  
+    })
+})
+
+// 2. Create and Send a New Post
+// ● Add a form with title and body fields
+// ● Use fetch() with the POST method to send the data as JSON to the API
+// ● Show a confirmation message with the response data
+
+const postForm = document.getElementById("postForm");//selecting dom elements
+const formSuccess = document.getElementById("formSuccess");
+//setting up eventlistener
+postForm.addEventListener('submit',(event) => {
+    event.preventDefault();//In order to prevent the default behavior of refreshing the page when the form is submitted.
+
+
+    const titleInput = document.getElementById('titleInput').value;//extracting the user input
+    const bodyInput = document.getElementById('bodyInput').value;
+    //to keep user input into this object
+    const newPostData = {
+        title: titleInput,
+        body: bodyInput,
+        userId: 1 
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPostData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+    })
+    .then(data => {
+        console.log('Success:', data);
+
+        formSuccess.innerHTML = `
+            <h3> Post Created</h3>
+            <p>${data.id}</p>
+            <p>${data.title}</p>
+            <p>${data.body}</p>
+        `;
+
+        postForm.reset();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        formSuccess.textContent = 'Failed to create post. Please try again.';
+    });
+});
+
+
+
